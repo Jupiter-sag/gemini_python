b55b0f6e3405586744c5a0b9a004144334beb99a
Jupiter-sag <shueiwang@hotmail.com>
2026-09-06 13:56:21 +0800

    變更

---
+++ 0906/prac3.py
@@ -0,0 +1,10 @@
+from telegram1 import Update
+
+def main():
+    u:Update = Update()
+    #print(u.message)
+    print(Update().message.reply_text("我是message實體的reply_text"))
+    
+if __name__ == "__main__":
+    main()
+    
